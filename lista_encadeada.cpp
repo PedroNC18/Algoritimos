57f70db29777cf7afc96901021ab65001532d3bc
PedroNC18 <39710987+PedroNC18@users.noreply.github.com>
2018-08-30 18:29:27 -0300

    Lista_encadeada
    Lista encadeada com todas as funções uteis

---
+++ lista_encadeada.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+
+using namespace std;
+
+struct lista{
+    int val;
+    lista *next;
+};
+
+bool list_end(lista *head){
+    if(head->next==nullptr){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+lista *create_node(int val){
+    lista *node=nullptr;
+    node = new lista;
+    node->val = val;
+    node->next = nullptr;
+    return node;
+}
+
+lista *create_node(int val, lista *nextval){
+    lista *node=nullptr;
+    node = new lista;
+    node->val = val;
+    node->next = nextval;
+    return node;
+}
+
+lista *insert_node(lista *head, int val){
+    lista *curr=nullptr;
+    curr = head;
+    while(!list_end(curr)){
+        curr = curr->next;
+    }
+    curr->next = create_node(val);
+    return head;
+}
+
+lista *insert_node(lista *head, int val, int pos){
+    lista *curr=nullptr;
+    curr = head;
+    while(pos--){
+        curr = curr->next;
+    }
+    curr->next = create_node(val,curr->next);
+    return head;
+}
+
+void show(lista *head){
+    lista *curr=nullptr;
+    curr = head;
+    while(!list_end(curr)){
+        curr = curr->next;
+        cout << curr->val << "->";
+    }
+    cout << "End-of-list" << endl;
+}
+
+lista *delete_node(lista *head){
+    lista *curr = nullptr;
+    lista *old = nullptr;
+    curr = head;
+    old = head;
+    while(!list_end(curr)){
+        old = curr;
+        curr = curr->next;
+    }
+    old->next = nullptr;
+    return head; 
+}
+
+lista *delete_node(lista *head, int pos){
+    lista *curr = nullptr;
+    lista *old = nullptr;
+    curr = head;
+    old = head;
+    curr = curr->next;
+    while(pos--){
+        //cout << "entrei" << endl;
+        old = curr;
+        curr = curr->next;
+    }
+    old->next = curr->next;
+    free(curr);
+    return head; 
+}
+
+int main(void){
+    lista *head=nullptr;
+    head = new lista;
+    head->next=nullptr;
+    int i;
+    for(i=1;i<6;i++){
+        head = insert_node(head, i);
+    }
+    show(head);
+    head = insert_node(head, 0, 0);
+    show(head);
+    head = delete_node(head);
+    show(head);
+    head = delete_node(head,1);
+    head = delete_node(head,2);
+    show(head);
+    free(head);
+    return 0;
+}
