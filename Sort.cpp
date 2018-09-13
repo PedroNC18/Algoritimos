662d3866f0fedeb1c9353de6ac9646ea8cddbbe3
PedroNC18 <39710987+PedroNC18@users.noreply.github.com>
2018-09-12 23:53:50 -0300

    Update Sort.cpp

--- Sort.cpp
+++ Sort.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-//SELECTION SORT
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  SELECTION SORT   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 void selection_sort(int vet[], int n){
 	int i, j, min;
 	for(i=0;i<n-1;i++){
@@ -17,8 +18,12 @@ void selection_sort(int vet[], int n){
 		vet[min] = aux;
 	}
 }
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-//INSERTION SORT
+
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  INSERTION SORT   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 void insertion_sort(int vet[],int n){
 	int i, j, v;
 	for(i=0;i<n;i++){
@@ -31,6 +36,59 @@ void insertion_sort(int vet[],int n){
 		vet[j+1] = v;
 	}
 }
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  MERGE SORT  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+void merge(int A[], int inicio, int meio, int fim){
+	int i=0, j=0, k=0;
+	int temp[100000], p1, p2, tam;
+	int fim1=0, fim2=0;
+	tam = fim-inicio+1;
+	p1 = inicio;
+	p2=meio+1;
+	for(i=0;i<tam;i++){
+		if(!fim1 && !fim2){
+			if(A[p1]<A[p2]){
+				temp[i] = A[p1++];
+			}
+			else{
+				temp[i] = A[p2++];
+			}
+			if(p1>meio){
+				fim1 = 1;
+			}
+			if(p2>fim){
+				fim2 = 1;
+			}
+		}
+		else{
+			if(!fim1){
+				temp[i] = A[p1++];
+			}
+			else{
+				temp[i] = A[p2++];
+			}
+		}
+	}
+	for(j=0,k=inicio;j<tam;j++,k++){
+		A[k] = temp[j];
+	}
+}
+
+void merge_sort(int A[], int inicio, int fim){
+	int meio;
+	if(inicio<fim){
+		meio = (inicio+fim)/2;
+		merge_sort(A,inicio,meio);
+		merge_sort(A,meio+1,fim);
+		merge(A,inicio,meio,fim);
+	}
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 int main() {
 	ios::sync_with_stdio(0);
