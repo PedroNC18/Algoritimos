cd372383cfa0958d7416e144e9d0ecd1b64400a8
PNC <39710987+PedroNC18@users.noreply.github.com>
2018-10-25 13:23:21 -0300

    Update Union_Find(array).cpp

--- Union_Find(array).cpp
+++ Union_Find(array).cpp
@@ -35,19 +35,6 @@ void join(int x, int y, vector<TIPE>&pai, vector<TIPE>&peso){
 }
  
 int main(){
-    int casos;
-    cin >> casos;
-    while(casos--){
-        vector<TIPE> pai, peso;
-        int p, n, aux1, aux2, maximo;
-        cin >> p >> n;
-        create_union(p, pai, peso);
-        while(n--){
-            cin >> aux1 >> aux2;
-            join(aux1, aux2, pai, peso);
-        }
-        maximo = decubra_max(n, pai);
-        cout << maximo <<  endl;
-    }
+    vector<TIPE> pai, peso;
     return 0;
 }
