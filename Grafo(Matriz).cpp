ae608dccabfb6c2357f66db7f81b1c18db7191e9
PNC <39710987+PedroNC18@users.noreply.github.com>
2018-10-27 15:02:13 -0300

    Update Grafo(Matriz).cpp

--- Grafo(Matriz).cpp
+++ Grafo(Matriz).cpp
@@ -1,32 +1,35 @@
 #include<iostream>
 #include<vector>
+#include<queue>
+#define NV 0
+#define VV 1
 using namespace std;
-
+ 
 struct Grafo{
     int **matrix;
     int numEgde;
     vector<int> mark;
 };
-
+ 
 Grafo create_grafo(int n){
     Grafo grafo;
     grafo.matrix = new int*[n];
     for(int i=0;i<n;i++){
-        grafo.matrix[i] = new int[n]; 
+        grafo.matrix[i] = new int[n];
     }
     grafo.numEgde=0;
     grafo.mark.resize(n);
     return grafo;
 }
-
+ 
 int v_number(Grafo& grafo){
     return grafo.mark.size();
 }
-
+ 
 int e_number(Grafo& grafo){
     return grafo.numEgde;
 }
-
+ 
 int first(Grafo& grafo, int a){
     int v_size = v_number(grafo);
     for(int i=0;i<v_size;i++){
@@ -36,35 +39,36 @@ int first(Grafo& grafo, int a){
     }
     return v_size;
 }
-
+ 
 void setEdge(Grafo& grafo, int a, int b, int p){
     if(p==0) cout << "Tu eh doido rapaz?" << endl;
     if(grafo.matrix[a][b]==0) grafo.numEgde++;
     grafo.matrix[a][b]=p;
+    grafo.matrix[b][a]=p;
 }
-
+ 
 void delEdge(Grafo& grafo, int a, int b){
     if(grafo.matrix[a][b]!=0) grafo.numEgde--;
     grafo.matrix[a][b]=0;
 }
-
+ 
 bool isEdge(Grafo& grafo, int a, int b){
     if(grafo.matrix[a][b]==0) return false;
     return true;
 }
-
+ 
 int peso(Grafo& grafo, int a, int b){
     return grafo.matrix[a][b];
 }
-
+ 
 void setMark(Grafo& grafo, int a, int val){
     grafo.mark[a]=val;
 }
-
+ 
 int getMark(Grafo& grafo, int a){
     return grafo.mark[a];
 }
-
+ 
 void print_matrix(Grafo grafo){
     int v_size=v_number(grafo);
     cout << "  ";
@@ -80,7 +84,49 @@ void print_matrix(Grafo grafo){
         cout << endl;
     }
 }
-
+ 
+int cont=0;
+ 
+void DFS(Grafo& grafo, int x){
+    int v_size=v_number(grafo);
+    for(int i=0;i<v_size;i++){
+        int v=grafo.matrix[x][i];
+        if(v!=0 && grafo.mark[i]==NV){
+            setMark(grafo, i, VV);
+            DFS(grafo,i);
+        }
+    }
+}
+ 
+void BFS(Grafo& grafo, int x){
+    queue<int> fila;
+    fila.push(x);
+    setMark(grafo, x, VV);
+    int i, atual=0, v_size=v_number(grafo);
+    while(!fila.empty()){
+        int v=fila.front();
+        fila.pop();
+        for(i=0;i<v_size;i++){
+            int u=grafo.matrix[v][i];
+            if(u!=0&&grafo.mark[i]==NV){
+                grafo.mark[i]=VV;
+                fila.push(i);
+            }
+        }
+    }
+}
+ 
+void graphTransverse(Grafo& grafo){
+    int v_size = v_number(grafo);
+    for(int i=0;i<v_size;i++) setMark(grafo, i, NV);
+    for(int i=0;i<v_size;i++){
+        if(getMark(grafo, i)==NV){
+            cont++;
+            DFS(grafo, i);    
+        } 
+    }
+}
+ 
 int main(void){
     Grafo grafo;
     int i, n, e, a, b, p;
@@ -97,6 +143,8 @@ int main(void){
     cout << "Numero de arestas: " << e_number(grafo) << endl;
     cout << "Numero de vertices: " << v_number(grafo) << endl;
     print_matrix(grafo);
-
+    graphTransverse(grafo);
+    cout << cont << endl;
+ 
     return 0;
 }
