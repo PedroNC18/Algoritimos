7ca81b37e0e68e8e5acd43c4b2ad806a6d68907c
PNC <39710987+PedroNC18@users.noreply.github.com>
2018-10-28 16:17:32 -0300

    Update and rename Grafo(Lista).cpp to Grafo(Lista)(-BFS,-DFS).cpp

--- Grafo(Lista)(-BFS,-DFS).cpp
+++ Grafo(Lista)(-BFS,-DFS).cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<queue>
 #define pb push_back
 #define new_vet vector<vector<pair<int,int>>>
 using namespace std;
  
 struct Grafo {
     int numEdge;
-    vector<int> mark;
+    vector<int> mark, grau;
     new_vet lista;
 };
  
@@ -15,6 +16,7 @@ void create_grafo(Grafo &grafo, int n){
     grafo.lista.resize(n);
     grafo.numEdge=0;
     grafo.mark.resize(n);
+    grafo.grau.resize(n);
 }
  
 int v_number(Grafo &grafo){
@@ -52,11 +54,13 @@ void setEdge(Grafo& grafo, int a, int b, int p){
     for( i=0;i<v_size;i++){
         if(grafo.lista[a][i].first==b){
             grafo.lista[a][i].second=p;
+            grafo.grau[b]++;
             break;
         }
     }
     if(i<v_size) return;
     grafo.lista[a].pb(make_pair(b, p));
+    grafo.grau[b]++;
     sort(grafo.lista[a].begin(), grafo.lista[a].end());
 }
  
@@ -116,6 +120,29 @@ void print_lista(Grafo& grafo, int n){
     }
 }
 
+void topo(Grafo& grafo){
+    vector<int> lista;
+    int v_size=v_number(grafo);
+    for(int i=0;i<v_size;i++) if(grafo.grau[i]==0) lista.pb(i);
+    int ini=0;
+    while(ini<lista.size()){
+        int atual= lista[ini];
+        ini++;
+        for(int i=0;i<grafo.lista[atual].size();i++){
+            int v=grafo.lista[atual][i].first;
+            grafo.grau[v]--;
+            if(grafo.grau[v]==0) lista.push_back(v);
+        }
+    }
+    if(lista.size()<v_number(grafo)) cout << "Deu merda" << endl;
+    else{
+        for(int i=0;i<v_size;i++){
+            cout << lista[i] << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main(void){
     Grafo grafo;
     int i, n, e, a, b, p;
@@ -125,13 +152,14 @@ int main(void){
     cin >> e;
     create_grafo(grafo, n);
     while(e--){
-        cout << "Digite a aresta e o seu peso: ";
-        cin >> a >> b >> p;
-        setEdge(grafo, a, b, p);
+        cout << "Digite a aresta: ";
+        cin >> a >> b;
+        setEdge(grafo, a, b, 1);
     }
     cout << "Numero de arestas: " << e_number(grafo) << endl;
     cout << "Numero de vertices: " << v_number(grafo) << endl;
     print_lista(grafo, n);
+    topo(grafo);
  
     return 0;
 }
